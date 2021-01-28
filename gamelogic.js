1202bae9f3d0d5ff8f41fee2cfaeb718dfaad47d
Frederiek107 <f.a.tenbroeke@gmail.com>
2021-01-28 18:21:31 +0100

    functions added

--- gamelogic.js
+++ gamelogic.js
@@ -1,13 +1,45 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+  let resultaat= "";
+  for (i=0; i<word.length; i++) {
+    let element = word[i];
+    const geraden = guesses.includes(element)
+        if (geraden) {
+          resultaat = resultaat + element + " ";
+        }
+        else {
+          resultaat = resultaat + "_ ";
+        }
+    }
+  return resultaat;
 }
 
 function isGameWon(word, guesses) {
-  // WRITE ME
-}
+  let resultaat=true;
+  for (i=0; i<word.length; i++) {
+    let element = word[i];
+    const geraden = guesses.includes(element);
+    if (!geraden) {
+      resultaat = false;
+      return resultaat;
+    }
+  }
+  return resultaat;
+    }
 
 function isGameLost(word, guesses) {
-  // WRITE ME
+  // 7 of meer foute letters ----> true
+  // 6 of minder foute letters ----> false
+  let resultaat=0;
+  for (i=0; i<guesses.length; i++) {
+    let element = guesses[i];
+    if (!word.includes(element)) {
+      resultaat ++;
+    }
+  }
+  if (resultaat>=7) {
+    return true;
+  }
+    return false;
 }
 
 module.exports = {
